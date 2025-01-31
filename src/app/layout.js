1b8ac1e37dbdd7a2de8622afc6426f2860943b81
jjjiL0ng <qweqwe9412@gmail.com>
2025-01-31 13:35:39 +0900

    hero section design fix and allign photo size

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Header from '@/components/layout/header'
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -30,6 +31,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Header />
         {children}
+        <Analytics />
       </body>
     </html>
   )
