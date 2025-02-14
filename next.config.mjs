e38c45acabfbdc02aabfb721f609b26b28cd0671
jjjiL0ng <qweqwe9412@gmail.com>
2025-02-14 14:38:02 +0900

    content block remove and improve security

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,34 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          source: '/:path*',
+          headers: [
+            {
+              key: 'Content-Security-Policy',
+              value: [
+                "default-src 'self'",
+                "script-src 'self' 'unsafe-inline' 'unsafe-eval' vitals.vercel-insights.com",
+                "style-src 'self' 'unsafe-inline'",
+                "img-src 'self' blob: data:",
+                "font-src 'self'",
+                "connect-src 'self' vitals.vercel-insights.com",
+                "frame-src 'self'"
+              ].join('; ')
+            }
+          ]
+        }
+      ]
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: '**'
+        }
+      ]
+    }
+  }
+  
+  export default nextConfig
