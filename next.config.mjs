9ea20c6d4e8e3dc58609be06d321e0877400d67d
jjjiL0ng <qweqwe9412@gmail.com>
2025-02-14 15:05:37 +0900

    footer link block and speed insight fix1

--- next.config.mjs
+++ next.config.mjs
@@ -9,11 +9,11 @@ const nextConfig = {
               key: 'Content-Security-Policy',
               value: [
                 "default-src 'self'",
-                "script-src 'self' 'unsafe-inline' 'unsafe-eval' vitals.vercel-insights.com *.vercel-insights.com *.vercel-scripts.com",
+                "script-src 'self' 'unsafe-inline' 'unsafe-eval' vitals.vercel-insights.com *.vercel-insights.com *.vercel-scripts.com va.vercel-scripts.com",
                 "style-src 'self' 'unsafe-inline'",
                 "img-src 'self' blob: data:",
                 "font-src 'self'",
-                "connect-src 'self' vitals.vercel-insights.com *.vercel-insights.com *.vercel-scripts.com",
+                "connect-src 'self' vitals.vercel-insights.com *.vercel-insights.com *.vercel-scripts.com va.vercel-scripts.com",
                 "frame-src 'self'"
               ].join('; ')
             }
