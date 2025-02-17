0e70dd638abd2c9eb019b2bd905f9587ca8b2b29
jjjiL0ng <qweqwe9412@gmail.com>
2025-02-17 11:44:48 +0900

    bug fix 3

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -57,9 +59,6 @@ function FirebaseAnalyticsWrapper({ children }) {
 }
 
 // 서버 컴포넌트
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
